74e47b4ff1e28ed8f0a21bf29f144545b0867e20
mayapeneva <maya.peneva@gmail.com>
2022-11-21 22:46:31 +0200

    Created IContentManager & StreamInfo, also initial ContentManager

---
+++ FileContentManagement/ContentManager/ContentManager.cs
@@ -0,0 +1,47 @@
+﻿namespace FileContentManagement
+{
+    using FileContentManagement.DTO;
+    using OneBitSoftware.Utilities;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class ContentManager<TKey> : IContentManager<TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        public Task<OperationResult> DeleteAsync(TKey id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<bool>> ExistsAsync(TKey id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<StreamInfo>> GetAsync(TKey id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<byte[]>> GetBytesAsync(TKey id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<string>> GetHashAsync(TKey id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> StoreAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> UpdateAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileContentManagement/ContentManager/IContentManager.cs
@@ -0,0 +1,26 @@
+﻿using FileContentManagement.DTO;
+using OneBitSoftware.Utilities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileContentManagement
+{
+    public interface IContentManager<in TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        Task<OperationResult> StoreAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken);
+
+        Task<OperationResult<bool>> ExistsAsync(TKey id, CancellationToken cancellationToken);
+
+        Task<OperationResult<StreamInfo>> GetAsync(TKey id, CancellationToken cancellationToken);
+
+        Task<OperationResult<byte[]>> GetBytesAsync(TKey id, CancellationToken cancellationToken);
+
+        Task<OperationResult> UpdateAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken);
+
+        Task<OperationResult> DeleteAsync(TKey id, CancellationToken cancellationToken);
+
+        Task<OperationResult<string>> GetHashAsync(TKey id, CancellationToken cancellationToken);
+    }
+}
