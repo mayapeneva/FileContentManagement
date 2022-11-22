17a3121352e1a736027dbbafd6f046f4a242c11c
mayapeneva <maya.peneva@gmail.com>
2022-11-22 14:17:10 +0200

    Added configuration file and validation of all its values

---
+++ FileContentManagement/Configration/FileManagementConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace FileContentManagement.Configuration
+{
+    internal class FileManagementConfiguration
+    {
+        public FileManagementConfiguration(string host, int port, string username, string password)
+        {
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ FileContentManagement/Configration/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace FileContentManagement.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        private const string SectionName = "FileManagement";
+        private const string Host = "host";
+        private const string Port = "port";
+        private const string Username = "username";
+        private const string Password = "password";
+
+        public static IServiceCollection RegisterContentManager<TKey>(this IServiceCollection services, IConfiguration configuration)
+        where TKey : struct, IEquatable<TKey>
+        {
+            var host = configuration[$"{SectionName}:{Host}"];
+            var isPortParsed = int.TryParse(configuration[$"{SectionName}:{Port}"], out int port);
+            var username = configuration[$"{SectionName}:{Username}"];
+            var password = configuration[$"{SectionName}:{Password}"];
+            if (string.IsNullOrWhiteSpace(host)
+                || !isPortParsed
+                || string.IsNullOrWhiteSpace(username)
+                || string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException(SectionName);
+            }
+
+            var fileManagementConfiguration = new FileManagementConfiguration (host, port, username, password);
+            services
+                .AddSingleton(fileManagementConfiguration)
+                .AddSingleton<IContentManager<TKey>, ContentManager<TKey>>();
+
+            return services;
+        }
+    }
+}
