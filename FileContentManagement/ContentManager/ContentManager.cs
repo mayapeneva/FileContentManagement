e3eadfef2447364f4e2e872953e0d7761e87ed6d
mayapeneva <maya.peneva@gmail.com>
2022-11-22 17:03:29 +0200

    Implemented GetAsync & GetBytesAsync methods in ContentManager. Small refactoring.

--- FileContentManagement/ContentManager/ContentManager.cs
+++ FileContentManagement/ContentManager/ContentManager.cs
@@ -13,6 +13,8 @@ namespace FileContentManagement
         where TKey : struct, IEquatable<TKey>
     {
         private readonly FileManagementConfiguration configuration;
+        private readonly string url;
+        private readonly NetworkCredential credentials;
 
         public ContentManager(FileManagementConfiguration configuration)
         {
@@ -22,6 +24,8 @@ namespace FileContentManagement
             }
 
             this.configuration = configuration;
+            url = $@"ftp://{configuration.Host}:{configuration.Port}/";
+            credentials = new NetworkCredential(configuration.Username, configuration.Password);
         }
 
         public async Task<OperationResult> StoreAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken)
@@ -29,17 +33,17 @@ namespace FileContentManagement
             var result = new OperationResult();
             if (fileContent.Stream is null)
             {
-                result.AppendError(string.Format(MessageConstants.StoringFail, id, "No file content to store."));
+                result.AppendError(string.Format(MessageConstants.StoringFailed, id, "No file content to store."));
                 return result;
             }
 
             try
             {
-                var uri = new Uri($@"ftp://{configuration.Host}:{configuration.Port}/{id}");
+                var uri = new Uri(url + id);
                 var request = (FtpWebRequest)WebRequest.Create(uri);
 
                 request.Method = WebRequestMethods.Ftp.UploadFile;
-                request.Credentials = new NetworkCredential(configuration.Username, configuration.Password);                
+                request.Credentials = credentials;                
                 request.ContentLength = fileContent.Stream.Length;
 
                 using Stream requestStream = request.GetRequestStream();
@@ -48,7 +52,7 @@ namespace FileContentManagement
                 using var response = (FtpWebResponse)request.GetResponse();
                 if (response.StatusCode != FtpStatusCode.FileActionOK)
                 {
-                    result.AppendError(string.Format(MessageConstants.StoringFail, id, response.StatusDescription));
+                    result.AppendError(string.Format(MessageConstants.StoringFailed, id, response.StatusDescription));
                     return result;
                 }
 
@@ -57,7 +61,7 @@ namespace FileContentManagement
             }
             catch (Exception ex)
             {
-                result.AppendError(string.Format(MessageConstants.StoringFail, id, ex.Message));
+                result.AppendError(string.Format(MessageConstants.StoringFailed, id, ex.Message));
                 return result;
             }
         }
@@ -66,11 +70,11 @@ namespace FileContentManagement
         {
             try
             {
-                var uri = new Uri($@"ftp://{configuration.Host}:{configuration.Port}/{id}");
+                var uri = new Uri(url + id);
                 var request = (FtpWebRequest)WebRequest.Create(uri);
 
                 request.Method = WebRequestMethods.Ftp.ListDirectory;
-                request.Credentials = new NetworkCredential(configuration.Username, configuration.Password);
+                request.Credentials = credentials;
 
                 using var response = (FtpWebResponse)request.GetResponse();
                 if (response != null)
@@ -83,19 +87,64 @@ namespace FileContentManagement
             catch (Exception ex)
             {
                 var result = new OperationResult<bool>(false);
-                result.AppendError(string.Format(MessageConstants.FileDoesNotExist, id, ex.Message));
+                result.AppendError(string.Format(MessageConstants.ExistsFailed, id, ex.Message));
                 return result;
             }
         }
 
-        public Task<OperationResult<StreamInfo>> GetAsync(TKey id, CancellationToken cancellationToken)
+        public async Task<OperationResult<StreamInfo>> GetAsync(TKey id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var uri = new Uri(url + id);
+                var request = (FtpWebRequest)WebRequest.Create(uri);
+
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = credentials;
+
+                var response = await request.GetResponseAsync();
+                var stream = response.GetResponseStream();
+                var streamInfo = new StreamInfo
+                {
+                    Length = stream.Length,
+                    Stream = stream
+                };
+
+                return new OperationResult<StreamInfo>(streamInfo);
+            }
+            catch (Exception ex)
+            {
+                var result = new OperationResult<StreamInfo>(new StreamInfo());
+                result.AppendError(string.Format(MessageConstants.GetFailed, id, ex.Message));
+                return result;
+            }
         }
 
-        public Task<OperationResult<byte[]>> GetBytesAsync(TKey id, CancellationToken cancellationToken)
+        public async Task<OperationResult<byte[]>> GetBytesAsync(TKey id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var uri = new Uri(url + id);
+                var request = (FtpWebRequest)WebRequest.Create(uri);
+
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = credentials;
+                request.KeepAlive = true;
+
+                var response = await request.GetResponseAsync();
+                var memoryStream = new MemoryStream();
+                await response.GetResponseStream().CopyToAsync(memoryStream, cancellationToken);
+
+                return new OperationResult<byte[]>(memoryStream.ToArray());
+            }
+            catch (Exception ex)
+            {
+                var result = new OperationResult<byte[]>(Array.Empty<byte>());
+                result.AppendError(string.Format(MessageConstants.GetFailed, id, ex.Message));
+                return result;
+            }
         }
 
         public Task<OperationResult> UpdateAsync(TKey id, StreamInfo fileContent, CancellationToken cancellationToken)
--- FileContentManagement/MessageConstants.cs
+++ FileContentManagement/MessageConstants.cs
@@ -4,8 +4,10 @@
     {
         public const string StoringSuccess = "The file with id {0} has been successfully stored.";
 
-        public const string StoringFail = "The file with id {0} was not stored. Reason: {1}";
+        public const string StoringFailed = "The file with id {0} was not stored. Reason: {1}";
 
-        public const string FileDoesNotExist = "A file with id {0} does not exist. Exception: {1}";
+        public const string ExistsFailed = "A file with id {0} does not exist. Exception: {1}";
+
+        public const string GetFailed = "A file with id {0} could not be downloaded. Exception: {1}";
     } 
 }
