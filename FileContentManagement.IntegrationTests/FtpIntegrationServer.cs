aebd45eba1823922a067b7e9814fc9cf0527ea41
mayapeneva <maya.peneva@gmail.com>
2022-11-23 15:39:28 +0200

    Added IntegrationTests project incl ftp set up from https://www.sentex.ca/~mwandel/ftpdmin/ . Added appsettings and registered the FtpContentManager

---
+++ FileContentManagement.IntegrationTests/FtpIntegrationServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FileContentManagement.IntegrationTests
+{
+    public class FtpIntegrationServer : IDisposable
+    {
+        private readonly Process ftpProcess;
+
+        public FtpIntegrationServer(string rootDirectory, int port = 21, bool hideFtpWindow = true)
+        {
+            var psInfo = new ProcessStartInfo
+            {
+                FileName = "ftpdmin.exe",
+                Arguments = String.Format("-p {0} -ha 127.0.0.1 \"{1}\"", port, rootDirectory),
+                WindowStyle = hideFtpWindow ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal
+            };
+
+            ftpProcess = Process.Start(psInfo);
+        }
+
+        public void Dispose()
+        {
+            if (!ftpProcess.HasExited)
+            {
+                ftpProcess.Kill();
+                ftpProcess.WaitForExit();
+            }
+        }
+    }
+}
---
+++ FileContentManagement.IntegrationTests/Program.cs
@@ -0,0 +1,29 @@
+﻿using FileContentManagement.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace FileContentManagement.IntegrationTests
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json",
+                    optional: false,
+                    reloadOnChange: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.RegisterContentManager<Guid>(hostContext.Configuration);
+                });
+    }
+}
